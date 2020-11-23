8687bf96ee6644e24923a6f71b33cb066441ca77
ChrisFlorez <christian.florez@despegar.com>
2020-11-23 12:00:06 -0300

    se agrega la funcionalidad de bloquear y desbloquear un usuario

--- src/controllers/users/Users.jsx
+++ src/controllers/users/Users.jsx
@@ -9,6 +9,10 @@ import TableContainer from '@material-ui/core/TableContainer';
 import Container from '@material-ui/core/Container';
 import TableBody from '@material-ui/core/TableBody'
 import tokenChecker from '../../utils/TokenChecker';
+import Button from '@material-ui/core/Button';
+import CheckIcon from '@material-ui/icons/CheckCircle'
+import BlockIcon from '@material-ui/icons/Block'
+import ProfileIcon from '@material-ui/icons/AccountCircle'
 import "./users.scss"
 
 const StyledTableCell = withStyles((theme) => ({
@@ -29,15 +33,6 @@ const StyledTableCell = withStyles((theme) => ({
     },
   }))(TableRow);
 
-  function createData(id, email, type, status, actions) {
-    return { id, email, type, status, actions };
-  }
-
-  const rows = [
-    createData(1, 'pp.argento@gmail.com', 'anfitrion', 'activo', 'acciones'),
-    createData(2, 'coki.argento@gmail.com', 'huesped', 'activo', 'acciones'),
-  ];
-
 class Users extends Component {
 
   constructor(props){
@@ -49,24 +44,27 @@ class Users extends Component {
     this.userTransform = this.userTransform.bind(this);
     this.usersTransform = this.usersTransform.bind(this);
     this.getUsers = this.getUsers.bind(this);
-    this.handleApiResponse = this.handleApiResponse.bind(this);
+    this.handleApiUsersResponse = this.handleApiUsersResponse.bind(this);
+    this.handleApiUserUpdateResponse = this.handleApiUserUpdateResponse.bind(this);
+    this.updateUserStatus = this.updateUserStatus.bind(this);
   }
 
   usersTransform(response) {
-    return response.map(user => this.userTransform(user));
+    return response.map(user => this.userTransform(user)).sort((a, b) => a.id - b.id);
   }
 
   userTransform(user) {
-    const state = user.is_block ? "bloqueado" : "activo"
+    const state = user.is_blocked ? "bloqueado" : "activo"
     return {
         id: user.id,
         email: user.email,
         type: user.type,
         state: state,
+        isBlock: user.is_blocked
     }
   }
 
-  handleApiResponse(response) {
+  handleApiUsersResponse(response) {
     if (response.error) {
       console.error("There was an error!", response.error)
       this.handleAlertStatus(response.error, 'error' )
@@ -75,6 +73,17 @@ class Users extends Component {
     else {
       this.setState( {usersData : this.usersTransform(response)} )
     }
+  }
+
+  handleApiUserUpdateResponse(response) {
+    if (response.error) {
+      console.error("There was an error!", response.error)
+      this.handleAlertStatus(response.error, 'error' )
+      this.props.history.push("/home");
+    }
+    else {
+      this.getUsers()
+    } 
   }
     
   getUsers() { 
@@ -89,7 +98,32 @@ class Users extends Component {
           'API_TOKEN': token
         }
       };
-      fetch(url, requestConfig).then(response => response.json()).then(this.handleApiResponse);
+      fetch(url, requestConfig).then(response => response.json()).then(this.handleApiUsersResponse);
+    }
+    else {
+      this.props.history.push("/");
+    }
+  }
+
+  updateUserStatus(userId, userStatus) {
+    const id = userId
+    const status = userStatus
+    if (tokenChecker()) {
+      const token = localStorage.getItem("token")
+      console.log("updating user_id: ", id)
+      const url =`https://facade-server-develop.herokuapp.com/users/${id}`
+      const requestConfig = {
+        method: 'PATCH',
+        body: JSON.stringify({
+          is_blocked: status
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          'API_TOKEN': token
+        }
+      };
+      
+      fetch(url, requestConfig).then(response => response.json()).then(this.handleApiUserUpdateResponse);
     }
     else {
       this.props.history.push("/");
@@ -119,13 +153,44 @@ class Users extends Component {
               <TableBody>
                 {users.map((row) => (
                   <StyledTableRow key={row.id}>
-                    <StyledTableCell component="th" scope="row">
-                      {row.id}
-                    </StyledTableCell>
+                    <StyledTableCell component="th" scope="row">{row.id}</StyledTableCell>
                     <StyledTableCell align="center">{row.email}</StyledTableCell>
                     <StyledTableCell align="center">{row.type}</StyledTableCell>
                     <StyledTableCell align="center">{row.state}</StyledTableCell>
-                    <StyledTableCell align="center">{row.actions}</StyledTableCell>
+                    <StyledTableCell align="center">
+                      <Button
+                        variant="contained"
+                        size="small"
+                        margin-right="4px"
+                        color="primary"
+                        className="button"
+                        startIcon={<ProfileIcon/>}
+                      >
+                        Ver perfil
+                      </Button>
+                    
+                      {!row.isBlock && <Button
+                        variant="contained"
+                        size="small"
+                        color="secondary"
+                        className="button"
+                        startIcon={<BlockIcon />}
+                        onClick={() => this.updateUserStatus(row.id, true)}
+                      >
+                        Bloquear
+                      </Button>}
+
+                      {row.isBlock &&
+                        <Button
+                          variant="contained"
+                          size="small"
+                          color="default"
+                          className="button"
+                          startIcon={<CheckIcon />}
+                          onClick={() => this.updateUserStatus(row.id, false)}>
+                          Desbloquear
+                        </Button>}
+                    </StyledTableCell>
                   </StyledTableRow>
                 ))}
               </TableBody>
